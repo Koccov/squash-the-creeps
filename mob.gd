62fe96cafbb3dec889684f53b7838df65b43ae1a
Koccov <gryzonin@gmail.com>
2023-10-03 20:54:28 +0200

    add movement logic to Mob

---
+++ mob.gd
@@ -0,0 +1,25 @@
+extends CharacterBody3D
+
+## Minimum speed of the mob [m/s]
+@export var min_speed = 10
+## Maximum speed of the mob [m/s]
+@export var max_speed = 18
+
+func _physics_process(delta):
+	move_and_slide()
+
+## to call from the Main scene
+func initialize(start_position, player_position):
+	# position the mob by placing it at start_position
+	# and rotate it towards player_position, so it looks at the player
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	
+	# rotate randomly between -90 and 90 degrees
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	
+	# calculate random speed
+	var random_speed = randi_range(min_speed, max_speed)
+	# calculate a forward velocity that represents the speed
+	velocity = Vector3.FORWARD * random_speed
+	# rotate velocity vector to align with Mob's look direction
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
