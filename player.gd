30c654c6129e2468244fcc8683864b706e803a6e
Koccov <gryzonin@gmail.com>
2023-10-04 21:56:15 +0200

    squash mobs when Player hits them from above

--- player.gd
+++ player.gd
@@ -6,6 +6,8 @@ extends CharacterBody3D
 @export var fall_acceleration = 75
 ## vertical impulse when jumping [m/s]
 @export var jump_impulse = 20
+## vertical impulse on bounce over a mob [m/s]
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
@@ -38,6 +40,24 @@ func _physics_process(delta):
 	# jumping
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		target_velocity.y = jump_impulse
+	
+	# iterate through all collision this frame
+	for index in range(get_slide_collision_count()):
+		# get one of the collisions
+		var collision = get_slide_collision(index)
+		
+		# if collision with ground
+		if (collision.get_collider() == null):
+			continue
+		
+		# if collision with mob
+		if (collision.get_collider().is_in_group("mob")):
+			var mob = collision.get_collider()
+			# check if hit from above
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# squash and bounce
+				mob.squash()
+				target_velocity.y = bounce_impulse
 	
 	# move the character
 	velocity = target_velocity
