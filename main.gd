c4d91722ab8f1edbb05cdd07423bd4eae61bb89a
Koccov <gryzonin@gmail.com>
2023-10-12 20:56:48 +0200

    add Retry screen

--- main.gd
+++ main.gd
@@ -2,7 +2,8 @@ extends Node
 
 @export var mob_scene: PackedScene
 
-
+func _ready():
+	$UserInterface/Retry.hide()
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
@@ -22,3 +23,8 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
